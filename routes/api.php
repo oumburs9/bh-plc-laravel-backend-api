1902875b07ed6461a097035816554e9b44393e4f
oum burs <oum.burs9mry@gmail.com>
2025-09-24 19:09:52 +0300

    added refresh token

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
 
     /*
     |--------------------------------------------------------------------------
